3dd4c60a8bddff3d67d60af64d0ce83268fb8867
Hussein Najah <husseinnajah123@gmail.com>
2022-12-03 15:59:09 +0300

    Somting

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,18 +5,19 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <script src="https://unpkg.com/windicss-runtime-dom"></script>
-
+    
     <title>{{ config('app.name', 'Laravel') }}</title>
-
+    
     <!-- Fonts -->
     <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Tajawal&display=swap" rel="stylesheet">
-
+    
     <!-- Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <script src="https://unpkg.com/windicss-runtime-dom"></script>
+    <script src="https://code.iconify.design/3/3.0.1/iconify.min.js"></script>
 
     <!-- Styles -->
     @livewireStyles
--- resources/views/livewire/ui/event-card.blade.php
+++ resources/views/livewire/ui/event-card.blade.php
@@ -1,3 +1,28 @@
-<div>
-    {{-- The best athlete wants his opponent at his best. --}}
+<div class="flex flex-col gap-10px p-20px w-340px h-350px rounded-15px shadow-lg bg-white relative">
+    {{-- Place Image --}}
+    <div class="w-300px h-150px bg-black rounded-md">
+    </div>
+    <div class="flex gap-30px">
+        {{-- Client Iamge --}}
+        <div class="w-90px h-90px rounded-full bg-red-100 absolute top-28 left-55"></div>
+        {{-- Cilent Name --}}
+        <span class="text-28px font-bold text-white absolute top-43 right-30 w-140%">احمد اسماعيل </span>
+    </div>
+
+    {{-- Client Description --}}
+    <span class="m-auto text-md font-semibold opacity-60">تعد مناقشة البحث العلمي المقدم من جانب الباحثين
+        العلميين هي سبيل الوصول إلى الدرجة العلمية.</span>
+
+    <div class="flex justify-between">
+        {{-- Event Date --}}
+        <div class="flex gap-5px justify-end items-center">
+            <span class="iconify text-secondery-100 text-20px" data-icon="ph:clock-fill"></span>
+            <span class="text-15px font-semibold text-black opacity-45"> 3:30 PM</span>
+        </div>
+        {{-- Event Location --}}
+        <div class="flex gap-5px justify-end items-center">
+            <span class="iconify text-secondery-100 text-20px" data-icon="mdi:google-maps"></span>
+            <span class="text-15px font-semibold text-black opacity-45"> جامعة البصرة - كلية العلوم</span>
+        </div>
+    </div>
 </div>
