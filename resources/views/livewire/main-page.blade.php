3a7329fcf886678bbc10647b4a77b318478c92af
Hussein Najah <husseinnajah123@gmail.com>
2022-12-03 12:23:29 +0300

    some studd

--- resources/views/livewire/main-page.blade.php
+++ resources/views/livewire/main-page.blade.php
@@ -1,2 +1,11 @@
-<div>
+<div class="mx-32 mt-44">
+    <div class="flex gap-24">
+        <div class="flex flex-col gap-10">
+            <span class="w-96 font-Tajawal text-justify text-3xl">يوفر لك النظام تصفح وارشفة الندوات والمؤتمرات ومناقشات طلبة
+                الدراسات العليا في كلية علوم الحاسوب و
+                تكنلوجيا المعلومات / جامعة البصرة من سنة 2023</span>
+                <input type="text" wire:model="search" placeholder="ابحث عن حدث.." class="w-96">
+            </div>
+            <div class="h-10 w-10 bg-black"></div>
+    </div>
 </div>
