70cb2b6bea26e4ce1b1c3911d4190b848d501b68
Hussein Najah <husseinnajah123@gmail.com>
2022-12-14 23:18:52 +0300

    Fixes
    Achieve Search bar.
    Navbar active class

--- resources/views/livewire/navbar.blade.php
+++ resources/views/livewire/navbar.blade.php
@@ -21,13 +21,17 @@
         </div>
         <div class="bx-5 ml-10 justify-between gap-3 font-semibold text-2xl">
             <a href="{{ route('main') }}">
-                <span class="p-2 cursor-pointer">الصفحة الرئيسية</span>
+                <span
+                    class="{{ request()->is('/') ? 'opacity-100' : '' }} p-2 cursor-pointer opacity-60 hover:opacity-100 duration-150 ">الصفحة
+                    الرئيسية</span>
             </a>
             <a href="{{ route('archive') }}">
-                <span class="p-2 text-black cursor-pointer">الارشيف</span>
+                <span
+                    class="{{ request()->is('archive') ? 'opacity-100' : '' }} p-2 text-black cursor-pointer opacity-60 hover:opacity-100 duration-150 ">الارشيف</span>
             </a>
             <a href="{{ route('about') }}">
-                <span class="p-2 text-black cursor-pointer">حول</span>
+                <span
+                    class="{{ request()->is('about') ? 'opacity-100' : '' }} p-2 text-black cursor-pointer opacity-60 hover:opacity-100 duration-150 ">حول</span>
             </a>
         </div>
     </div>
