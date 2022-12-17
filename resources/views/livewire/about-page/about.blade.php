a2f349d5918e319b1dba2530bcf08fef219011e1
Hussein Najah <husseinnajah123@gmail.com>
2022-12-17 16:46:53 +0300

    Some UI Fixes

--- resources/views/livewire/about-page/about.blade.php
+++ resources/views/livewire/about-page/about.blade.php
@@ -1,9 +1,9 @@
 <div>
     <div>
         <div class="flex justify-between mt-24">
-            <div>
+            <div class="mt-15">
                 <span class="text-3xl font-medium">
-                    نظام <span class="font-bold text-5xl"> مكتبة السمنرات</span> الألكتروني, يتيح لك الفرصة لانشاء الندوات, المؤتمرات و مناقشات البحث العلمية.
+                    نظام <span class="text-5xl font-bold"> مكتبة السمنرات</span> الألكتروني, يتيح لك الفرصة لانشاء الندوات, المؤتمرات و مناقشات البحث العلمية.
                 </span>
             </div>
             <img src="/image/About-Page-Image.png" alt="About-Page" class="h-[500px]">
