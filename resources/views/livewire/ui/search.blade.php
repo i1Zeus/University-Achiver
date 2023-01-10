fcd6eda43b8313c03150ef1dc9ce7b51fb4e53b5
Hussein Najah <husseinnajah123@gmail.com>
2023-01-10 20:17:48 +0300

    Fixes

--- resources/views/livewire/ui/search.blade.php
+++ resources/views/livewire/ui/search.blade.php
@@ -1,5 +1,5 @@
 <div>
-    <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
+    <label for="search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
     <div class="relative">
         <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
             <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor"
@@ -8,8 +8,8 @@
                     d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
             </svg>
         </div>
-        <input type="search" id="default-search" wire:model="search"
-            class="block p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-700px bg-gray-50 focus:ring-secondery-100 focus:border-secondery-100 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-secondery-100 dark:focus:border-secondery-100"
+        <input type="search" id="search" wire:model="search"
+            class="block p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-700px bg-gray-50 focus:ring-secondery-100 focus:border-secondery-100"
             placeholder="ابحث عن حدث .." required>
     </div>
 </div>
