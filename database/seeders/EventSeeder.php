d100f71be5f7b85092f8a0350848a027ed1f5fe9
Hussein Najah <husseinnajah123@gmail.com>
2023-01-23 17:04:42 +0300

    seeder info

--- database/seeders/EventSeeder.php
+++ database/seeders/EventSeeder.php
@@ -17,36 +17,63 @@ class EventSeeder extends Seeder
     {
         //1
         Event::create([
-            'title' => 'Event 1',
-            'description' => 'Event 1 description',
-            'state' => 1,
-            'address' => 'Address 1',
-            'time'=>'2021-01-01',
-            'start' => '2021-01-01',
-            'end' => '2021-01-01',
-            'time' => '8:30'
+            'title' => 'حسين جاسم فهد',
+            'description' => 'Analyze to loT with Learning Deep of lmplementation and Design
+            Cit Smart Datain Bi',
+            'state' => 1, //مناقشه بحث
+            'address' => 'قاعه الدراسات العليا',
+            'start' => '2023-01-11',
+            'time' => '9:00'
         ]);
         //2
         Event::create([
-            'title' => 'Event 2',
-            'description' => 'Event 2 description',
+            'title' => 'علاء ضاحي خليفه',
+            'description' => 'Detection of IOT Botnet Cyber Attacks
+            Using Machine Learning',
             'state' => 1,
-            'address' => 'Address 2',
-            'time'=>'09:30',
-            'start' => '2021-01-01',
-            'end' => '2021-01-01',
+            'address' => 'قاعه الدراسات العليا',
+            'start' => '2023-1-16',
+            'time' => '10:00'
         ]);
-
         //3
         Event::create([
-            'title' => 'Event 3',
-            'description' => 'Event 3 description',
-            'state' => 1,
-            'address' => 'Address 3',
-
-            'time'=>'09:30',
-            'start' => '2021-01-01',
-            'end' => '2021-01-01',
+            'title' => 'فاطمه باسم كاظم',
+            'description' => 'for model implementation and Design
+            asthma Detection supported ontology building
+            disease',
+            'state' => 1, //مناقشه بحث
+            'address' => 'قاعه الدراسات العليا',
+            'start' => '2023-01-10',
+            'time' => '10:30'
+        ]);
+        //4
+        Event::create([
+            'title' => 'هيام عباس قاسم',
+            'description' => "E-HeaIthcare Patient's for Authentication
+            Privacy and Safety :System",
+            'state' => 1, //مناقشه بحث
+            'address' => 'قاعه الدراسات العليا',
+            'start' => '2022-12-27',
+            'time' => '10:15'
+        ]);
+        //5
+        Event::create([
+            'title' => 'جواد كاظم حسين',
+            'description' => 'Computer Network Monitoring to Performance Evaluation for Quality of Services',
+            'state' => 1, //مناقشه بحث
+            'address' => 'قاعه الدراسات العليا',
+            'start' => '2022-12-21',
+            'time' => '10:00'
+        ]);
+        //6
+        Event::create([
+            'title' => 'حسين جاسم فهد',
+            'description' => 'CONGESTION BASED BALANCING LOAD TRAFFIC
+            NEN/ORKS SPEED HIGH IN AVOIDING',
+            'state' => 1, //مناقشه بحث
+            'address' => 'قاعه الدراسات العليا',
+            'start' => '2022-12-21',
+            'time' => '10:15'
         ]);
     }
 }
