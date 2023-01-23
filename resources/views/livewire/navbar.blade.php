ea44acc474e9efde0310ee17474ca11407722054
Hussein Najah <husseinnajah123@gmail.com>
2023-01-23 15:49:45 +0300

    nav

--- resources/views/livewire/navbar.blade.php
+++ resources/views/livewire/navbar.blade.php
@@ -7,28 +7,33 @@
             </div>
             <span class="text-5xl font-bold text-white">مكتبة السمنرات</span>
         </div>
-        <div class="justify-between gap-3 ml-10 text-2xl font-semibold bx-5">
+        <div class="flex items-center justify-between gap-3 ml-10 text-2xl font-semibold bx-5">
             <a href="{{ route('main') }}">
-                <span class="{{ request()->is('/') ? '!opacity-100' : '' }} p-2 cursor-pointer opacity-60 hover:opacity-100 duration-150 ">الصفحة
+                <span
+                    class="{{ request()->is('/') ? '!opacity-100' : '' }} p-2 cursor-pointer opacity-60 hover:opacity-100 duration-150 ">الصفحة
                     الرئيسية</span>
             </a>
             <a href="{{ route('archive') }}">
-                <span class="{{ request()->is('archive') ? '!opacity-100' : '' }} p-2 text-black cursor-pointer opacity-60 hover:opacity-100 duration-150 ">الارشيف</span>
+                <span
+                    class="{{ request()->is('archive') ? '!opacity-100' : '' }} p-2 text-black cursor-pointer opacity-60 hover:opacity-100 duration-150 ">الارشيف</span>
             </a>
             <a href="{{ route('about') }}">
-                <span class="{{ request()->is('about') ? '!opacity-100' : '' }} p-2 text-black cursor-pointer opacity-60 hover:opacity-100 duration-150 ">حول</span>
+                <span
+                    class="{{ request()->is('about') ? '!opacity-100' : '' }} p-2 text-black cursor-pointer opacity-60 hover:opacity-100 duration-150 ">حول</span>
             </a>
             <!-- hidden if user is logged in -->
-
-            <a href="{{ route('login') }}">
-                <span class="{{ request()->is('login') ? '!opacity-100' : '' }} @if (Auth::check()) hidden @endif
-                     p-2 text-black cursor-pointer opacity-60 hover:opacity-100 duration-150 ">تسجيل دخول</span>
-            </a>
-                <form method="POST" action="{{ route('logout') }}" class="p-2 text-black cursor-pointer opacity-60 hover:opacity-100 duration-150 @if (!Auth::check()) hidden @endif" x-data>
+            @if (!Auth::check())
+                <a href="{{ route('login') }}" class="flex items-center duration-150 cursor-pointer opacity-60 hover:opacity-100 ">
+                    @livewire('components.login')
+                </a>
+            @else
+                <form method="POST" action="{{ route('logout') }}" x-data>
                     @csrf
-                    <button type="submit" class=" ">
-                        تسجيل خروج </button>
+                    <button type="submit" class="flex items-center duration-150 cursor-pointer opacity-60 hover:opacity-100">
+                        @livewire('components.logout')
+                    </button>
                 </form>
+            @endif
         </div>
     </div>
 </div>
