3453e082c60d54fe1d848cf0236c12761e368be6
Hussein Najah <husseinnajah123@gmail.com>
2023-01-23 16:13:31 +0300

    logo

--- resources/views/auth/verify-email.blade.php
+++ resources/views/auth/verify-email.blade.php
@@ -1,7 +1,9 @@
 <x-guest-layout>
     <x-jet-authentication-card>
         <x-slot name="logo">
-            <x-jet-authentication-card-logo />
+            <div class="justify-center ml-[28%] w-[40%]">
+                @livewire('components.logo')
+            </div>
         </x-slot>
 
         <div class="mb-4 text-sm text-gray-600">
@@ -9,12 +11,12 @@
         </div>
 
         @if (session('status') == 'verification-link-sent')
-            <div class="mb-4 font-medium text-sm text-green-600">
+            <div class="mb-4 text-sm font-medium text-green-600">
                 {{ __('A new verification link has been sent to the email address you provided in your profile settings.') }}
             </div>
         @endif
 
-        <div class="mt-4 flex items-center justify-between">
+        <div class="flex items-center justify-between mt-4">
             <form method="POST" action="{{ route('verification.send') }}">
                 @csrf
 
@@ -26,16 +28,13 @@
             </form>
 
             <div>
-                <a
-                    href="{{ route('profile.show') }}"
-                    class="underline text-sm text-gray-600 hover:text-gray-900"
-                >
+                <a href="{{ route('profile.show') }}" class="text-sm text-gray-600 underline hover:text-gray-900">
                     {{ __('Edit Profile') }}</a>
 
                 <form method="POST" action="{{ route('logout') }}" class="inline">
                     @csrf
 
-                    <button type="submit" class="underline text-sm text-gray-600 hover:text-gray-900 ml-2">
+                    <button type="submit" class="ml-2 text-sm text-gray-600 underline hover:text-gray-900">
                         {{ __('Log Out') }}
                     </button>
                 </form>
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -15,10 +15,26 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
-<body>
-    <div class="font-sans text-gray-900 antialiased">
-        {{ $slot }}
+<body class="antialiased font-Tajawal bg-background-200">
+    <x-jet-banner />
+
+    <div class="min-h-screen bg-gray-100">
+        <!-- Page Content -->
+        <main class="relative mx-20 mt-10">
+            {{ $slot }}
+        </main>
     </div>
+
+    @stack('modals')
+    <script src="https://unpkg.com/flowbite@1.5.4/dist/flowbite.js"></script>
+    @livewireScripts
+    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <script src="{{ asset('vendor/livewire-alert/livewire-alert.js') }}"></script>
+    <script src="https://unpkg.com/windicss-runtime-dom"></script>
+    <script src="https://code.iconify.design/3/3.0.1/iconify.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/js/all.min.js"
+        integrity="sha512-6PM0qYu5KExuNcKt5bURAoT6KCThUmHRewN3zUFNaoI6Di7XJPTMoT6K0nsagZKk2OB4L7E3q1uQKHNHd4stIQ=="
+        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 </body>
 
 </html>
