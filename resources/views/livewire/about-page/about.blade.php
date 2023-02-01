87862a380bfbc67bdc5b999b6834c7ec0b23cb48
Hussein Najah <husseinnajah123@gmail.com>
2023-02-01 20:39:20 +0300

    final touches

--- resources/views/livewire/about-page/about.blade.php
+++ resources/views/livewire/about-page/about.blade.php
@@ -1,11 +1,44 @@
 <div>
-    <div>
+    <div class="flex flex-col relative">
         <div class="flex justify-between mt-24">
             <div class="mt-15">
                 <span class="text-3xl font-medium">
                     نظام <span class="text-5xl font-bold"> مكتبة السمنرات</span> الألكتروني, يتيح لك الفرصة لانشاء
                     الندوات, المؤتمرات و مناقشات البحث العلمية.
                 </span>
+                <div class="my-6">
+                    <h2 class="text-3xl font-medium">فريقنا</h2>
+                    <div class="flex flex-wrap justify-end -mx-2" dir="ltr">
+                        <div class="w-1/3 px-2 mb-4">
+                            <div
+                                class="p-4 duration-150 border-[3px] border-dashed rounded-lg hover:border-solid hover:border-primary-200">
+                                <img class="w-[70px] mx-auto mb-4 bg-cover rounded-full" src="/image/reda.jpg"
+                                    alt="team member">
+                                <h3 class="text-lg font-medium">رضا عامر باسر</h3>
+                                <p class="text-gray-600">Web Developer</p>
+                                <a href="mailto:it192210019@avicenna.uobasrah.edu.iq"
+                                    class="inline-block mx-2 text-black opacity-50 duration-150 hover:opacity-100"
+                                    target="_blank" rel="noopener">
+                                    @livewire('components.email')
+                                </a>
+                            </div>
+                        </div>
+                        <div class="w-1/3 px-2 mb-4">
+                            <div
+                                class="p-4 duration-150 border-[3px] border-dashed rounded-lg hover:border-solid hover:border-primary-200">
+                                <img class="w-[70px] h-16 mx-auto mb-4 rounded-full" src="/image/ali.jpg"
+                                    alt="team member">
+                                <h3 class="text-lg font-medium">علي بشير عبيد</h3>
+                                <p class="text-gray-600">Web Developer</p>
+                                <a href="mailto:it192210029@avicenna.uobasrah.edu.iq"
+                                    class="inline-block mx-2 text-black opacity-50 duration-150 hover:opacity-100"
+                                    target="_blank" rel="noopener">
+                                    @livewire('components.email')
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
             <img src="/image/About-Page-Image.png" alt="About-Page" class="h-[500px]">
         </div>
